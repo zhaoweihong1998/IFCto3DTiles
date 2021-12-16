727f440d74f2ee73df46534f7233e17e36b52faa
Monto <1491172660@qq.com>
2021-12-16 12:21:32 +0800

    modified create path

--- JavaInterface/src/run.sh
+++ JavaInterface/src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=~/3DTiles/JavaInterface/lib:$LD_LIBRARY_PATH
-java -Djava.library.path=../lib App ../ifc/room.ifc
+export LD_LIBRARY_PATH=../../build/lib:../../build/bin/Release:../../build/bin/Debug:$LD_LIBRARY_PATH
+java -Djava.library.path=../../build/lib:../../build/bin/Release:../../build/bin/Debug App ../ifc/room.ifc
